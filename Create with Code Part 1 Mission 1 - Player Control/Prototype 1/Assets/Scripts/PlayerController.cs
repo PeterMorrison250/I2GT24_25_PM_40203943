09bcd7c8dae7830410716cc3a98679dfc90d7990
PeterMorrison250 <petermorrison250@gmail.com>
2024-09-24 00:57:23 +0100

    Add steering

--- Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 20;
+    private float forwardInput;
+    private float horizontalInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // Move the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
