c48ab10e3578ac453f34b8e57781d2a8ce380814
PeterMorrison250 <petermorrison250@gmail.com>
2024-10-29 22:57:31 +0000

    Add Gravity Direction Enums

---
+++ Project/Gyrobo/Assets/Scripts/Enums/GravityDirections.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gyrobo.Enums
+{
+    public enum GravityDirections
+    {
+        DOWN = 0,
+        RIGHT = 1,
+        UP = 2,
+        LEFT = 3,
+    }
+}
