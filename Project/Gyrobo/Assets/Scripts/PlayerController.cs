d46dac13e8da92ee5f140649d678f9cb5de58d34
PeterMorrison250 <petermorrison250@gmail.com>
2024-10-29 23:25:30 +0000

    Refactored jumping so that it works no matter what gravitational orientation

--- Project/Gyrobo/Assets/Scripts/PlayerController.cs
+++ Project/Gyrobo/Assets/Scripts/PlayerController.cs
@@ -8,27 +8,62 @@ namespace Gyrobo
     public class PlayerController : MonoBehaviour
     {
         public float horizontalInput;
+        public float verticalInput;
 
         public float playerSpeed = 5;
 
         public Rigidbody rigidBody;
 
+        public GravityController gravityController;
+
         // Start is called before the first frame update
         void Start()
         {
             rigidBody = GetComponent<Rigidbody>();
+            gravityController = GetComponent<GravityController>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            horizontalInput = Input.GetAxis("Horizontal");
-            var distance = horizontalInput * playerSpeed * Time.deltaTime;
-            transform.Translate(Vector3.right * distance);
+            if (gravityController.gravityDirection == Enums.GravityDirections.UP || gravityController.gravityDirection == Enums.GravityDirections.DOWN)
+            {
+                horizontalInput = Input.GetAxis("Horizontal");
+                var distance = horizontalInput * playerSpeed * Time.deltaTime;
+                transform.Translate(Vector3.right * distance);
+            } else
+            {
+                verticalInput = Input.GetAxis("Vertical");
+                var distance = verticalInput * playerSpeed * Time.deltaTime;
+                transform.Translate(Vector3.up * distance);
+            }
+
+            
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                rigidBody.AddForce(new Vector3(0,5,0), ForceMode.Impulse);
+                ApplyGravityDirection();
+            }
+        }
+
+        private void ApplyGravityDirection()
+        {
+            switch (gravityController.gravityDirection)
+            {
+                case Enums.GravityDirections.DOWN:
+                    rigidBody.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+                    break;
+                case Enums.GravityDirections.RIGHT:
+                    rigidBody.AddForce(new Vector3( -5, 0, 0), ForceMode.Impulse);
+                    break;
+                case Enums.GravityDirections.UP:
+                    rigidBody.AddForce(new Vector3(0, -5, 0), ForceMode.Impulse);
+                    break;
+                case Enums.GravityDirections.LEFT:
+                    rigidBody.AddForce(new Vector3(5, 0, 0), ForceMode.Impulse);
+                    break;
+                default:
+                    break;
             }
         }
     }
