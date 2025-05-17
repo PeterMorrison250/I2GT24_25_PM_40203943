35eccd7267a6ed4e995d023e888c01d433aca9ae
Peter Morrison <petermorrison250@gmail.com>
2025-05-17 10:28:59 +0100

    Add gravity inversion to helium cube

--- Project/Gyrobo/Assets/Scripts/InvertGravity.cs
+++ Project/Gyrobo/Assets/Scripts/InvertGravity.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class InvertGravity : MonoBehaviour
 {
+    public Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(-(Physics.gravity / 15), ForceMode.Acceleration);
     }
 }
