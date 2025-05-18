a7ebde833afdc09adc3b8ed47386aa3a58706149
Peter Morrison <petermorrison250@gmail.com>
2025-05-18 10:32:16 +0100

    Refine collision detection to prevent hitting sides of walls resetting jump detection

---
+++ Project/Gyrobo/Assets/Scripts/GravityExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Gyrobo.Enums;
+using UnityEngine;
+
+namespace Gyrobo
+{
+    public static class GravityExtensions
+    {
+        public static Vector3 ToVector3(this GravityDirection gravityDirection)
+        {
+            switch (gravityDirection)
+            {
+                case GravityDirection.Up:
+                    return Vector3.up;
+                case GravityDirection.Down:
+                    return Vector3.down;
+                case GravityDirection.Left:
+                    return Vector3.left;
+                case GravityDirection.Right:
+                    return Vector3.right;
+            }
+            return Vector3.zero;
+        }
+
+        public static Vector3 Invert(this Vector3 vector)
+        {
+            return -vector;
+        }
+    }
+}
