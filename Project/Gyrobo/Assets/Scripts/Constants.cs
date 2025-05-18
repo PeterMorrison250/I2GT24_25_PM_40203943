873037b5901ab6d795d8f1011754c9513b9387ea
Peter Morrison <petermorrison250@gmail.com>
2025-05-18 11:21:55 +0100

    Add initial laser emitter

--- Project/Gyrobo/Assets/Scripts/Constants.cs
+++ Project/Gyrobo/Assets/Scripts/Constants.cs
@@ -11,7 +11,7 @@
         public const float PlatformSpeed = 2;
 
         public const float Level1LeftBoundary = 20f;
-        public const float Level1RightBoundary = -20f;
+        public const float Level1RightBoundary = -50f;
         public const float Level1TopBoundary = 40f;
         public const float Level1BottomBoundary = 0f;
     }
