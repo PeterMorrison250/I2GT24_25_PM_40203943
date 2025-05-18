3bad3eb8e4fb1cecb2f2ea131b23239924a5c556
Peter Morrison <petermorrison250@gmail.com>
2025-05-18 11:38:30 +0100

    Damage player if touching the laser beam

--- Project/Gyrobo/Assets/Scripts/LaserController.cs
+++ Project/Gyrobo/Assets/Scripts/LaserController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Gyrobo;
 using UnityEngine;
 
@@ -7,10 +8,17 @@ public class LaserController : MonoBehaviour
 {
     public LineRenderer lineRenderer;
     
+    private GameObject _gameManagerObject;
+    private GameManager _gameManager;
+    private PlayerController _playerController;
+    
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        _gameManagerObject = GameObject.FindGameObjectsWithTag("GameManager").FirstOrDefault();
+        _gameManager = _gameManagerObject?.GetComponent<GameManager>();
+        _playerController = _gameManager.player.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -22,6 +30,11 @@ public class LaserController : MonoBehaviour
         if (Physics.Raycast(transform.position, Vector3.up, out hit))
         {
             lineRenderer.SetPosition(1, hit.point);
+
+            if (hit.transform.tag == "Player")
+            {
+                _playerController.DamagePlayer();
+            }
         }
         else
         {
