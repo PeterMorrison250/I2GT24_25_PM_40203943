781c3f5d2f04293d8d4bbda7e624bc853f6af9db
Peter Morrison <petermorrison250@gmail.com>
2025-05-19 12:07:02 +0100

    Update all level boundaries

--- Project/Gyrobo/Assets/Scripts/LevelBoundaryManager.cs
+++ Project/Gyrobo/Assets/Scripts/LevelBoundaryManager.cs
@@ -7,9 +7,12 @@ namespace Gyrobo
         public static Dictionary<string, LevelBoundary> LevelBoundaryDictionary = new Dictionary<string, LevelBoundary>()
         {
             {"Sandbox",  new LevelBoundary{MinX = 20, MaxX = -20, MinY = -20, MaxY = 20}},
-            {"Level1",  new LevelBoundary{MinX = 20, MaxX = -70, MinY = -20, MaxY = 20}},
-            {"Level2",  new LevelBoundary{MinX = 20, MaxX = -30, MinY = -20, MaxY = 20}},
+            {"Corridor",  new LevelBoundary{MinX = 20, MaxX = -70, MinY = -20, MaxY = 20}},
+            {"LaserButton",  new LevelBoundary{MinX = 20, MaxX = -30, MinY = -20, MaxY = 20}},
             {"Elevator", new LevelBoundary{MinX = 20, MaxX = -30, MinY = 0, MaxY = 60}},
+            {"HeliumElevator", new LevelBoundary{MinX = 20, MaxX = -80, MinY = 0, MaxY = 100}},
+            {"TitleScreen", new LevelBoundary{MinX = 20, MaxX = -30, MinY = 0, MaxY = 60}},
+            {"GravityLaser", new LevelBoundary{MinX = 40, MaxX = -30, MinY = 0, MaxY = 60}},
         };
     }
 }
