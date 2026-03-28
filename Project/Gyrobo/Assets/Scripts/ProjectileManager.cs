86ce4133db0e6b8a47fcdb29cb101db2446e0067
Peter Morrison <petermorrison250@gmail.com>
2026-03-28 11:31:19 +0000

    Destroy projectile when it comes into contact with a rigidbody

--- Project/Gyrobo/Assets/Scripts/ProjectileManager.cs
+++ Project/Gyrobo/Assets/Scripts/ProjectileManager.cs
@@ -1,18 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected void OnCollisionEnter(Collision other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(gameObject);
     }
 }
