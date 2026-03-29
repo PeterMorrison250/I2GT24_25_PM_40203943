321d6a1d24e21d5892a9fb6a53dcff7c76a2f1c1
Peter Morrison <petermorrison250@gmail.com>
2026-03-29 18:59:43 +0100

    Add initial ray cast to see if the player is in front of the chaser

--- Project/Gyrobo/Assets/Scripts/ChaserManager.cs
+++ Project/Gyrobo/Assets/Scripts/ChaserManager.cs
@@ -6,16 +6,37 @@ public class ChaserManager : MonoBehaviour
 {
     [SerializeField] private float range;
     [SerializeField] private Transform playerTransform;
-
-
+    
+    private LayerMask _layerMask;
+    
+    private void Awake()
+    {
+        _layerMask = LayerMask.GetMask("Player");
+    }
+    
     private void FixedUpdate()
     {
         if (!IsInRange)
         {
             return;
         }
+
+        TrackPlayer();
     }
     
     private bool IsInRange => Vector3.Distance(playerTransform.position, transform.position) <= range;
 
+    private void TrackPlayer()
+    {
+        var moveDirection = (playerTransform.position - transform.position).normalized;
+
+        if (Physics.Raycast(transform.position, moveDirection, out var hit, range, _layerMask))
+        {
+            Debug.DrawRay(transform.position, moveDirection * hit.distance, Color.yellow);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, moveDirection * 1000, Color.white);
+        }
+    }
 }
