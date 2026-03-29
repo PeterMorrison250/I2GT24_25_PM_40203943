866b1031f6dbc8ce629ebbff1dd4899fc0bafd37
Peter Morrison <petermorrison250@gmail.com>
2026-03-29 19:04:00 +0100

    Connect frontTrackerTransform into the ChaserManager.cs

--- Project/Gyrobo/Assets/Scripts/ChaserManager.cs
+++ Project/Gyrobo/Assets/Scripts/ChaserManager.cs
@@ -6,6 +6,7 @@ public class ChaserManager : MonoBehaviour
 {
     [SerializeField] private float range;
     [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform frontTrackerTransform;
     
     private LayerMask _layerMask;
     
